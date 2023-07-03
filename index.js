44806241adbf509369e7556c770c8f67dcb00df9
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2023-07-03 17:33:33 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const stringRandomizer = (string) => {
+  if (!string) throw new Error("No value provided.");
+  if (typeof string !== "string") throw new Error("The provided value is not an string.");
+  let randomizedString = [];
+  let strLen = string.length;
+  for (let i = 0; i < strLen; i++) {
+    let randomNum = Math.floor(Math.random() * string.length);
+    randomizedString.push(string[randomNum]);
+    string = string.slice(0, randomNum) + string.slice(randomNum + 1);
+  }
+  return randomizedString.join("");
+};
